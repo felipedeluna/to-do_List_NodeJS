57d5d701c56454586889225b59ecf290a8287bb1
Felipe de Luna <fcoladodeluna@gmail.com>
2024-10-15 02:10:05 +0000

    Configuração e solução de erros em ambiente de produção relacionado a conexão com o redis

--- backend/config/redis.js
+++ backend/config/redis.js
@@ -4,6 +4,4 @@ const redisClient = redis.createClient({
     url: 'redis://localhost:6379'
 })
 
-redisClient.connect()
-
 module.exports = redisClient
--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,23 @@
+
 const app = require('./app');
 const connectMongo = require('./config/mongo');
 const redisClient = require('./config/redis');
 
 const PORT = process.env.PORT || 3000;
 
-(async () => {
-    try {
-        await connectMongo()
-        console.log('MongoDB conectado');
+connectMongo().then(() => {
+    console.log('MongoDB conectado com sucesso!')
 
-        await redisClient.connect()
-        console.log('Redis conectado');
-
-        // Inicia o app
-        app.listen(PORT, () => {
-            console.log(`Servidor rodando na porta ${PORT}`);
-        });
-    } catch (error) {
-        console.error('Erro ao inicializar o servidor:', err);
+    redisClient.connect().then(() => {
+	console.log('Redis conectado com sucesso!')
+	app.listen(PORT, () => {
+        console.log(`Servidor rodando na porta ${PORT}`);
+    });
+    }).catch(() => {
+	console.error('Erro ao conectar ao redis:', err);
         process.exit(1);
-    }
+    })
+}).catch((err) => {
+    console.error('Erro ao conectar ao MongoDB:', err);
+    process.exit(1);
 })
